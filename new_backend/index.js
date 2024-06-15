6edf88346099fe94be0dc6ae8dbf9509b742f65f
Alec Hajinoff <hajinoff@gmail.com>
2024-06-15 15:47:09 +0100

    Commit one

---
+++ new_backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const logger = require('morgan');
+const jobsRouter = require('./jobs');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/jobs', jobsRouter);
+module.exports = app;
+app.listen(8080);
+console.log("App listening on port 8080");
